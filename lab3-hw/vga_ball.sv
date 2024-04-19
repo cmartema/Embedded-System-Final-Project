95eb9c64b6a4306ec12ab5bd4ac895f35bd65493
Somya Mehta <somya@Somya-Mehtas-MacBook-Pro.local>
2024-04-19 13:25:23 -0400

    more syntax

--- lab3-hw/vga_ball.sv
+++ lab3-hw/vga_ball.sv
@@ -59,24 +59,42 @@ module vga_ball(
   reg [7:0] a;
   reg [7:0] b;
   reg [7:0] c;
-  
+  /*
    always_comb begin
     {VGA_R, VGA_G, VGA_B} = {8'h0, 8'h0, 8'h0};
-    /*
-    if (VGA_BLANK_n) begin
-	    if (hcount[10:6] == 5'b1010 && vcount[9:5] == 5'b1010) begin
-        a = {3'b0, apple_sprite_output[15:11]};
-        b = {2'b0, apple_sprite_output[10:5]};
-        c = {3'b0, apple_sprite_output[4:0]};
-        {VGA_R, VGA_G, VGA_B} = {a, b, c};
+    if (VGA_BLANK_n)
 
-	    end
-	    else begin
-	      {VGA_R, VGA_G, VGA_B} ={background_r, background_g, background_b};
-      end
+    if (hcount[10:6] == 5'b1010 && vcount[9:5] == 5'b1010) 
+      a = {3'b0, apple_sprite_output[15:11]};
+      b = {2'b0, apple_sprite_output[10:5]};
+      c = {3'b0, apple_sprite_output[4:0]};
+      {VGA_R, VGA_G, VGA_B} = {a, b, c};
+
+    
+    else 
+      {VGA_R, VGA_G, VGA_B} ={background_r, background_g, background_b};
+    
     end
     */
-  end
+
+    always_comb begin
+      {VGA_R, VGA_G, VGA_B} = {8'h0, 8'h0, 8'h0};
+      if (VGA_BLANK_n )
+  /*
+	if (hcount[10:6] == 5'b1010 &&
+	    vcount[9:5] == 5'b1010)
+	  a = {3'b0, apple_sprite_output[15:11]};
+    b = {2'b0, apple_sprite_output[10:5]};
+    c = {3'b0, apple_sprite_output[4:0]};
+    {VGA_R, VGA_G, VGA_B} = {a, b, c};
+	else
+	  {VGA_R, VGA_G, VGA_B} =
+             {background_r, background_g, background_b};
+  */
+   end
+   
+    
+  
    
    
 	
