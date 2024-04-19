f1dfe5be7542266894e90ca43c7540dbd02f4bb8
Somya Mehta <somya@Somya-Mehtas-MacBook-Pro.local>
2024-04-19 12:22:01 -0400

    syntax?

--- lab3-hw/vga_ball.sv
+++ lab3-hw/vga_ball.sv
@@ -55,18 +55,27 @@ module vga_ball(
        endcase
 
   //logic for generating vga output
-  
+  reg [4:0] x;
+  reg [5:0] y;
+  reg [4:0] z;
    always_comb begin
-      {VGA_R, VGA_G, VGA_B} = {8'h0, 8'h0, 8'h0};
-      if (VGA_BLANK_n )
+    {VGA_R, VGA_G, VGA_B} = {8'h0, 8'h0, 8'h0};
+      if (VGA_BLANK_n ) begin
 	if (hcount[10:6] == 5'b1010 && vcount[9:5] == 5'b1010) begin
-	   // {VGA_R, VGA_G, VGA_B} = { {apple_sprite_output[15:11], 3b'000},  {apple_sprite_output[10:5], 2b'00}, {apple_sprite_output[4:0], 3b'000} };  
-	   {VGA_R, VGA_G, VGA_B} = {apple_sprite_output[15:11],  apple_sprite_output[10:5], apple_sprite_output[4:0]};
+	   x = {3'b0, apple_sprite_output[15:11]};
+	   y = {2'b0, apple_sprite_output[10:5]};
+	   z = {3'b0, apple_sprite_output[4:0]};
+     {VGA_R, VGA_G, VGA_B} = {x, y, z};
+	   //{VGA_R, VGA_G, VGA_B} = { {apple_sprite_output[15:11], 3'b0},  {apple_sprite_output[10:5], 2'b00}, {apple_sprite_output[4:0], 3b'000} };  
+	   //{VGA_R, VGA_G, VGA_B} = {apple_sprite_output[15:11],  apple_sprite_output[10:5], apple_sprite_output[4:0]};
 	  end
-	else
+	else begin
 	  {VGA_R, VGA_G, VGA_B} =
              {background_r, background_g, background_b};
    end
+   end
+   end
+   
 	
   
        /*
