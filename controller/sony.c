7c197fd5e601a6b10d210057ca9d65c698ea5466
Somya Mehta <somya@Somya-Mehtas-MacBook-Pro.local>
2024-05-04 14:29:09 -0400

    new sony.c

--- controller/sony.c
+++ controller/sony.c
@@ -18,9 +18,13 @@
  * The argument con
  * 
  */
-struct libusb_device_handle *openkeyboard(uint8_t *endpoint_address) {
+#define VENDOR_ID 0x054c  // Sony Corporation
+#define PRODUCT_ID_1 0x0ce6  // Product ID for the first controller
+
+
+struct libusb_device_handle *opensony(uint8_t *endpoint_address) {
   libusb_device **devs;
-  struct libusb_device_handle *keyboard = NULL;
+  struct libusb_device_handle *sony = NULL;
   struct libusb_device_descriptor desc;
   ssize_t num_devs, d;
   uint8_t i, k;
@@ -38,7 +42,7 @@ struct libusb_device_handle *openkeyboard(uint8_t *endpoint_address) {
   }
 
   /* Look at each device, remembering the first HID device that speaks
-     the keyboard protocol */
+     the sony protocol */
 
   for (d = 0 ; d < num_devs ; d++) {
     libusb_device *dev = devs[d];
@@ -47,45 +51,60 @@ struct libusb_device_handle *openkeyboard(uint8_t *endpoint_address) {
       exit(1);
     }
 
-    if (desc.bDeviceClass == LIBUSB_CLASS_PER_INTERFACE) {
+    //search for device
+    if (desc.idVendor == VENDOR_ID && (desc.idProduct == PRODUCT_ID_1)) {
+      printf("device found\n");
       struct libusb_config_descriptor *config;
-      libusb_get_config_descriptor(dev, 0, &config);
-      for (i = 0 ; i < config->bNumInterfaces ; i++)	       
-	for ( k = 0 ; k < config->interface[i].num_altsetting ; k++ ) {
-	  const struct libusb_interface_descriptor *inter =
-	    config->interface[i].altsetting + k ;
-	  if ( inter->bInterfaceClass == LIBUSB_CLASS_HID &&
-	       inter->bInterfaceProtocol == USB_HID_KEYBOARD_PROTOCOL) {
-	    int r;
-	    if ((r = libusb_open(dev, &keyboard)) != 0) {
-	      fprintf(stderr, "Error: libusb_open failed: %d\n", r);
-	      exit(1);
-	    }
-	    if (libusb_kernel_driver_active(keyboard,i))
-	      libusb_detach_kernel_driver(keyboard, i);
-	    libusb_set_auto_detach_kernel_driver(keyboard, i);
-	    if ((r = libusb_claim_interface(keyboard, i)) != 0) {
-	      fprintf(stderr, "Error: libusb_claim_interface failed: %d\n", r);
-	      exit(1);
-	    }
-	    *endpoint_address = inter->endpoint[0].bEndpointAddress;
-	    goto found;
-	  }
-	}
+      if (libusb_get_config_descriptor(dev, 0, &config) < 0) {
+          fprintf(stderr, "Error: libusb_get_config_descriptor failed\n");
+          continue;  // Skip to the next device
+      }
+    
+
+      if (desc.bDeviceClass == LIBUSB_CLASS_PER_INTERFACE) {
+        struct libusb_config_descriptor *config;
+        libusb_get_config_descriptor(dev, 0, &config);
+        for (i = 0 ; i < config->bNumInterfaces ; i++)	{       
+          for ( k = 0 ; k < config->interface[i].num_altsetting ; k++ ) {
+            const struct libusb_interface_descriptor *inter =
+              config->interface[i].altsetting + k ;
+            if ( inter->bInterfaceClass == LIBUSB_CLASS_HID) {
+              int r;
+              printf("found HID at configuration %d\b", i);
+              if ((r = libusb_open(dev, &sony)) != 0) {
+                fprintf(stderr, "Error: libusb_open failed: %d\n", r);
+                exit(1);
+              }
+              if (libusb_kernel_driver_active(sony,i)){
+                libusb_detach_kernel_driver(sony, i);
+                libusb_set_auto_detach_kernel_driver(sony, i);
+              }
+              if ((r = libusb_claim_interface(sony, i)) != 0) {
+                fprintf(stderr, "Error: libusb_claim_interface failed: %d\n", r);
+                exit(1);
+              }
+            }
+          }
+        }
+      }
+    *endpoint_address = inter->endpoint[0].bEndpointAddress;
+    goto found;
+	  
+	
     }
   }
 
  found:
   libusb_free_device_list(devs, 1);
 
-  return keyboard;
+  return sony;
 }
 
 int main() {
 	uint8_t endpoint_address;
-	struct libusb_device_handle *keyboard;
-	if ((keyboard = openkeyboard(&endpoint_address)) == NULL ) {
-		fprintf(stderr, "Did not find keyboard\n");
+	struct libusb_device_handle *sony;
+	if ((sony = opensony(&endpoint_address)) == NULL ) {
+		fprintf(stderr, "Did not find sony\n");
 		exit(1);
 	}	
 	return 0;
