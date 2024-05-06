b18a93d355697724a35f4f98c4764fc822077f66
Somya Mehta <somya@Somya-Mehtas-MacBook-Pro.local>
2024-05-06 19:28:01 -0400

    move fruit logic

--- lab3-sw/hello.c
+++ lab3-sw/hello.c
@@ -379,8 +379,12 @@ int main()
     } else if (direction == 0x06){
         vla.coordinate.x -= 1;
     } else if (direction == 0x00) {
+        printf("%d, %d\n", vla.coordinate.x, vla.coordinate.y);
+        printf("%d, %d\n", head_up.coordinate.x, head_up.coordinate.y);
         head_up.coordinate.y = vla.coordinate.y;
         head_up.coordinate.x = vla.coordinate.x;
+        printf("%d, %d\n", vla.coordinate.x, vla.coordinate.y);
+        printf("%d, %d\n", head_up.coordinate.x, head_up.coordinate.y);
         //vla.coordinate.x -= 1;
        // vla.coordinate.y -= 1;
     } else if (direction == 0x04) {
