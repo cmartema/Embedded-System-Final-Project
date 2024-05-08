57ee8024ec6534e88591a626c714c0ad20ea694e
Somya Mehta <somya@Somya-Mehtas-MacBook-Pro.local>
2024-05-08 17:27:19 -0400

    test

--- lab3-hw/vga_ball.sv
+++ lab3-hw/vga_ball.sv
@@ -214,7 +214,7 @@ always_ff @(posedge clk) begin
 
       //snake head right
       
-      else if (hcount[10:5] == (x_pos[5:0]-1) && hcount[4:1] >= 4'b1111 && vcount[9:4] == y_pos[5:0] && sprite_type == 8'b10) begin //coordinates(10,10) 31
+      if (hcount[10:5] == (x_pos[5:0]-1) && hcount[4:1] >= 4'b1111 && vcount[9:4] == y_pos[5:0] && sprite_type == 8'b10) begin //coordinates(10,10) 31
         snake_head_right_sprite_addr <= hcount[4:1] - 4'b1111 + (vcount[3:0])*16;
         a <= {snake_head_right_sprite_output[15:11], 3'b0};
         b <= { snake_head_right_sprite_output[10:5], 2'b0};
