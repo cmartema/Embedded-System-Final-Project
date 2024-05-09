6334ec350f6ff434bdab4b54a0d440060f9e22b4
Somya Mehta <somya@Somya-Mehtas-MacBook-Pro.local>
2024-05-09 16:41:43 -0400

    retesting old commit

--- lab3-sw/hello.c
+++ lab3-sw/hello.c
@@ -327,11 +327,11 @@ int main()
 
 
     // set_ball_coordinate(&vla.coordinate, &fruit.coordinate, &head_up.coordinate);
-/*
+
     unsigned short int x = 10;
     unsigned short int y = 10;
     unsigned short int map = 1;
-    vla.coordinate_and_map.x = x;
+/*    vla.coordinate_and_map.x = x;
     vla.coordinate_and_map.y = y;
     vla.coordinate_and_map.map = map;
     set_ball_coordinate(&vla);
