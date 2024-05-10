a2b007225f31e77de2836ce386e8dc5ab2f0a8b9
Somya Mehta <somya@Somya-Mehtas-MacBook-Pro.local>
2024-05-10 15:31:02 -0400

    added leading zeros

--- lab3-sw/hello.c
+++ lab3-sw/hello.c
@@ -338,6 +338,7 @@ int main()
     
     x = 20;
     y = 20;
+    /*
     map = 2;
     vla.coordinate_and_map.x = x;
     vla.coordinate_and_map.y = y;
@@ -352,7 +353,7 @@ int main()
     vla.coordinate_and_map.y = y;
     vla.coordinate_and_map.map = map;
     set_ball_coordinate(&vla);
-
+*/
     /*
     direction = 0x08;
 
