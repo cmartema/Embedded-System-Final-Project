c58c9f2891f2d6bc992b35df289e8c3c9fdac783
rohanxxx <mrabbi@fordham.edu>
2024-05-11 03:42:57 -0400

    theory testing with hardware

--- lab3-sw/hello.c
+++ lab3-sw/hello.c
@@ -293,12 +293,12 @@ int main()
         }
     }*/
     
-    vla.grid.data = combine(1,0,0,0);  
+    vla.grid.data = combine(1,1,1,1);  
     vla.grid.offset = 10;  
     set_ball_coordinate(&vla.grid);
 
-    vla.grid.data = combine(2,0,0,0); 
-    vla.grid.offset = 15;   
+    vla.grid.data = combine(2,2,2,2); 
+    vla.grid.offset = 20;   
     set_ball_coordinate(&vla.grid);
 
     // vla.grid.data = combine(1,0,0,0); 
