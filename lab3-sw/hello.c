5f3121bc6cf70f85d52da80d3ec75f72a8c268da
rohanxxx <mrabbi@fordham.edu>
2024-05-11 14:46:17 -0400

    theory testing with hardware

--- lab3-sw/hello.c
+++ lab3-sw/hello.c
@@ -294,7 +294,17 @@ int main()
     }*/
     int count = 0;
     // vla.grid.offset = 0;
+    /*
     for (int i = 0; i < 27; i++){
+        vla.grid.data = combine(0,0,1,1);  
+        vla.grid.offset = count;  
+        set_ball_coordinate(&vla.grid);
+        count += 40;
+        printf("count: %d\n", count);
+    }
+    */
+    count = 36;   
+    for (int i = 0; i < 26; i++){
         vla.grid.data = combine(0,0,1,1);  
         vla.grid.offset = count;  
         set_ball_coordinate(&vla.grid);
