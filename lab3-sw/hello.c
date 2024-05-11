2d17ef9f1f62b2ad5cf1f158459db2089e293559
rohanxxx <mrabbi@fordham.edu>
2024-05-11 17:29:30 -0400

    testing

--- lab3-sw/hello.c
+++ lab3-sw/hello.c
@@ -300,7 +300,7 @@ int main()
     int count = 0;
     for(int i = 0; i < 30; i++){
         vla.grid.data = combine(1,1,1,1);  
-        vla.grid.offset = 0;
+        vla.grid.offset = count;
         set_ball_coordinate(&vla.grid); 
         count += 40; 
     }
