1035a52c96ea5bc907bc0d987c61ffb62e9be3cf
rohanxxx <mrabbi@fordham.edu>
2024-05-11 21:46:20 -0400

    trying to fix delay when displaying sprites

--- lab3-hw/vga_ball.sv
+++ lab3-hw/vga_ball.sv
@@ -558,9 +558,22 @@ module vga_ball(
   always_ff @(posedge clk) begin
     if (VGA_BLANK_n) begin
       // Dynamic Sprites
+      /*
+      if (hcount[10:5] == (d[5:0]-1) && hcount[4:1] >= 4'b1111 && vcount[9:4] == e[5:0]) begin //coordinates(10,10) 31
+        apple_sprite_addr <= hcount[4:1] - 4'b1111 + (vcount[3:0])*16;
+        a <= {apple_sprite_output[15:11], 3'b0};
+        b <= { apple_sprite_output[10:5], 2'b0};
+        c <= {apple_sprite_output[4:0], 3'b0};
+      end else if (hcount[10:5] == d[5:0] && hcount[4:1] < 4'b1111 && vcount[9:4] == e[5:0]) begin
+        apple_sprite_addr <=  hcount[4:1] + 4'b0001 + (vcount[3:0])*16;
+        a <= {apple_sprite_output[15:11], 3'b0};
+        b <= { apple_sprite_output[10:5], 2'b0};
+        c <= {apple_sprite_output[4:0], 3'b0};
+      end
+      */
       // apple - 1
       if (map[hcount[10:5]][vcount[9:4]] == 8'b1) begin 
-        apple_sprite_addr <= hcount[4:1] + (vcount[3:0])*16;
+        apple_sprite_addr <= hcount[4:1] - 4'b111 + (vcount[3:0])*16;
         a <= {apple_sprite_output[15:11], 3'b0};
         b <= {apple_sprite_output[10:5], 2'b0};
         c <= {apple_sprite_output[4:0], 3'b0};
@@ -588,7 +601,7 @@ module vga_ball(
       end
       // head right - 5
       else if (map[hcount[10:5]][vcount[9:4]] == 8'b101) begin 
-        snake_head_right_sprite_addr <= hcount[4:1] + (vcount[3:0])*16;
+        snake_head_right_sprite_addr <= hcount[4:1] - 4'b111 + (vcount[3:0])*16;
         a <= {snake_head_right_sprite_output[15:11], 3'b0};
         b <= {snake_head_right_sprite_output[10:5], 2'b0};
         c <= {snake_head_right_sprite_output[4:0], 3'b0};
@@ -602,7 +615,7 @@ module vga_ball(
       end
       // body horizontal - 7
       else if (map[hcount[10:5]][vcount[9:4]] == 8'b111) begin 
-        snake_body_horizontal_sprite_addr <= hcount[4:1] + (vcount[3:0])*16;
+        snake_body_horizontal_sprite_addr <= hcount[4:1] - 4'b111 + (vcount[3:0])*16;
         a <= {snake_body_horizontal_sprite_output[15:11], 3'b0};
         b <= {snake_body_horizontal_sprite_output[10:5], 2'b0};
         c <= {snake_body_horizontal_sprite_output[4:0], 3'b0};
@@ -651,7 +664,7 @@ module vga_ball(
       end
       // tail left - 14
       else if (map[hcount[10:5]][vcount[9:4]] == 8'b1110) begin 
-        snake_tail_left_sprite_addr <= hcount[4:1] + (vcount[3:0])*16;
+        snake_tail_left_sprite_addr <= hcount[4:1] - 4'b111 + (vcount[3:0])*16;
         a <= {snake_tail_left_sprite_output[15:11], 3'b0};
         b <= {snake_tail_left_sprite_output[10:5], 2'b0};
         c <= {snake_tail_left_sprite_output[4:0], 3'b0};
